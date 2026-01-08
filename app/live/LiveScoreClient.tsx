8b58db373c57f64ebc3167f38574f91702af2541
Viet Chu <viet40@gmail.com>
2026-01-08 16:37:55 -0600

    Aligned 'yd' to baseline with number using items-baseline

--- app/live/LiveScoreClient.tsx
+++ app/live/LiveScoreClient.tsx
@@ -789,8 +789,8 @@ export default function LiveScoreClient({ allPlayers, defaultCourse, initialRoun
 
                             return (
                                 <div className="bg-green-600 text-white py-3 rounded-lg text-center mb-2 shadow-inner">
-                                    <p className="font-black text-[90pt] flex items-end justify-end gap-2 pr-4">
-                                        {dist} <span className="text-[20pt] pb-1">yd</span>
+                                    <p className="font-black text-[90pt] flex items-baseline justify-end gap-2 pr-4">
+                                        {dist} <span className="text-[20pt]">yd</span>
                                     </p>
                                 </div>
                             );
