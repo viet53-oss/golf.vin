106ae9136b12b0e709fe542679bbc0f807044f4c
Viet Chu <viet40@gmail.com>
2026-01-10 18:25:48 -0600

    Further increase player scoring font sizes (30pt, 50pt, 30pt)

--- app/live/LiveScoreClient.tsx
+++ app/live/LiveScoreClient.tsx
@@ -1455,18 +1455,18 @@ export default function LiveScoreClient({ allPlayers, defaultCourse, initialRoun
                                                     {canUpdate && (
                                                         <button
                                                             onClick={() => updateScore(player.id, false)}
-                                                            className="w-12 h-12 rounded-full bg-[#ff3b30] flex items-center justify-center text-white font-bold shadow-md active:scale-95 transition-transform text-[15pt]"
+                                                            className="w-12 h-12 rounded-full bg-[#ff3b30] flex items-center justify-center text-white font-bold shadow-md active:scale-95 transition-transform text-[30pt]"
                                                         >
                                                             -
                                                         </button>
                                                     )}
-                                                    <div className="w-16 text-center font-bold text-[35pt] text-gray-800">
+                                                    <div className="w-16 text-center font-bold text-[50pt] text-gray-800">
                                                         {score || <span className="text-gray-800">{activeHolePar}</span>}
                                                     </div>
                                                     {canUpdate && (
                                                         <button
                                                             onClick={() => updateScore(player.id, true)}
-                                                            className="w-12 h-12 rounded-full bg-[#00c950] flex items-center justify-center text-white font-bold shadow-md active:scale-95 transition-transform text-[15pt]"
+                                                            className="w-12 h-12 rounded-full bg-[#00c950] flex items-center justify-center text-white font-bold shadow-md active:scale-95 transition-transform text-[30pt]"
                                                         >
                                                             +
                                                         </button>
