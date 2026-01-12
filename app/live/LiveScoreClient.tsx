0037e94c998e0663e68c30327a52558e3396db14
Viet Chu <viet40@gmail.com>
2026-01-11 20:04:06 -0600

    Increase GPS font size to 90pt

--- app/live/LiveScoreClient.tsx
+++ app/live/LiveScoreClient.tsx
@@ -1311,7 +1311,7 @@ export default function LiveScoreClient({ allPlayers, defaultCourse, initialRoun
                                                 {renderElement('RIGHT', 2, 'top-2 right-2')}
                                                 {renderElement('RIGHT', 1, 'bottom-2 right-2')}
 
-                                                <p className="font-black text-[70pt] leading-none flex items-center justify-center pt-2 pb-4">
+                                                <p className="font-black text-[90pt] leading-none flex items-center justify-center pt-2 pb-4">
                                                     {dist}
                                                 </p>
                                             </div>
